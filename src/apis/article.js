ce35c30d6ae0275fc4299bba2b2e41b367f0d8e5
胡伟东 <153321594@qq.com>
2022-04-07 20:25:08 +0800

    v3
    dd

--- src/apis/article.js
+++ src/apis/article.js
@@ -1,35 +1,34 @@
-// import $api from './index'
+import $api from './index'
 
-// // 发布公告
-// export const addNoticle = async (data) => {
-//   const res = await $api.post('/article/add', data)
-//   return res
-// }
+// 发布公告
+export const addNoticle = async (data) => {
+  const res = await $api.post('/article/add', data)
+  return res
+}
+export const deleteNoticle = async (data) => {
+  return await $api.post('/article/delete', data)
+}
 
-// export const deleteNoticle = async (data) => {
-//   return await $api.post('/article/delete', data)
-// }
+export const updateNoticle = async (data) => {
+  return await $api.post('/article/update', data)
+}
 
-// export const updateNoticle = async (data) => {
-//   return await $api.post('/article/update', data)
-// }
+export const getId = async (data) => {
+  return await $api.post('/article/getId', data)
+}
 
-// export const getId = async (data) => {
-//     return await $api.post('/article/getId', data)
-// }
+export const getTitle = async (data) => {
+  return await $api.post('/article/getTitle', data)
+}
 
-// export const getTitle = async (data) => {
-//     return await $api.post('/article/getTitle', data)
-// }
+export const getCate = async (data) => {
+  return await $api.post('/article/getCate', data)
+}
 
-// export const getCate = async (data) => {
-//     return await $api.post('/article/getCate', data)
-// }
+export const getContent = async (data) => {
+  return await $api.post('/article/getContent', data)
+}
 
-// export const getContent = async (data) => {
-//     return await $api.post('/article/getContent', data)
-// }
-
-// export const getAllNoticle = async (data) => {
-//   return await $api.post('/article/getAll', data)
-// }
+export const getAllNoticle = async (data) => {
+  return await $api.post('/article/getAll', data)
+}
--- src/views/notice/sendNotice.vue
+++ src/views/notice/sendNotice.vue
@@ -1,150 +1,112 @@
 <template>
   <d2-container>
-    <el-form ref="form" 
+    <el-form ref='form'
     :model="form"
     label-position="left"
     label-width="100px">
       <el-form-item label="公告主题">
-        <el-input v-model="form.name" placeholder="请输入内容"></el-input>
+        <el-input v-model="form.title" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="接受网点">
-        <div @click="site=true">
-            <el-input v-model="form.name" placeholder="请选择"></el-input>
-        </div>
+      <el-form-item label="分类">
+        <el-radio-group size="small">
+          <el-radio-button :label="1">寄件</el-radio-button>
+          <el-radio-button :label="2">收件</el-radio-button>
+          <el-radio-button :label="3">费用</el-radio-button>
+          <el-radio-button :label="4">招聘</el-radio-button>
+        </el-radio-group>
       </el-form-item>
-      <el-form-item label="接收员工">
+      <el-form-item label="内容">
         <div @click="staff=true">
-            <el-input v-model="form.name" placeholder="请选择"></el-input>
+            <el-input v-model="form.content" placeholder="请输入"></el-input>
         </div>
       </el-form-item>
       <el-form-item label="封面：">
         <el-upload
-          class="upload-demo"
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :before-remove="beforeRemove"
-          multiple
-          :limit="3"
-          :on-exceed="handleExceed"
-          :file-list="fileList"
         >
-          <el-button size="small" type="primary">选取图片</el-button>
+          <el-button size="small" type="primary">选取封面</el-button>
         </el-upload>
-      </el-form-item>
-      <el-form-item label="是否必读">
-        <el-radio v-model="radio" label="1">是</el-radio>
-        <el-radio v-model="radio" label="2">否</el-radio>
       </el-form-item>
       <el-button type="primary" @click="sendClick">发送</el-button>
     </el-form>
-    <!-- 接受网点弹窗 -->
+    <!-- 接受分类弹窗 -->
     <el-dialog
-        title="选择接受网点"
+        title="分类内容"
         :visible.sync="site"
-        width="950px"
-        :before-close="handleClose">
-        <el-input class="keyWord" v-model="form.name" placeholder="输入关键字进行过滤"></el-input>
-        <el-tree
-            :props="props"
-            :load="loadNode"
-            lazy
-            show-checkbox>
-        </el-tree>
+        width="950px">
         <span slot="footer" class="dialog-footer">
             <el-button @click="site= false">取 消</el-button>
-            <el-button type="primary" @click="rejectSite = false">确 定</el-button>
-        </span>
-    </el-dialog>
-    <!-- 接受员工弹窗 -->
-    <el-dialog
-        title="选择接受员工"
-        :visible.sync="staff"
-        width="950px"
-        :before-close="handleClose">
-        <el-input class="keyWord" v-model="form.name" placeholder="输入关键字进行过滤"></el-input>
-        <el-tree
-            :props="props"
-            :load="loadNode"
-            lazy
-            show-checkbox>
-        </el-tree>
-        <span slot="footer" class="dialog-footer">
-            <el-button @click="staff= false">取 消</el-button>
             <el-button type="primary" @click="rejectSite = false">确 定</el-button>
         </span>
     </el-dialog>
   </d2-container>
 </template>
 <script>
-// import { page } from '@vue/composition-api'
+import { getAllNoticle, getTitle } from '@/apis/article'
 export default {
-  name: "sendNotice",
-  data() {
+  name: 'sendNotice',
+  data () {
     return {
       page: 1,
       limit: 20,
       tableDate: [],
       total: 0,
-      form: {},
+      form: {
+        title: '',
+        cate: '',
+        content: '',
+        cover: ''
+      },
       props: {
-          label: 'name',
-          children: 'zones',
-          isLeaf: 'leaf'
-        },
+        label: 'name',
+        children: 'zones',
+        isLeaf: 'leaf'
+      },
       site: false,
       staff: false
-    };
+    }
+  },
+  created () {
+    this.getList()
   },
   methods: {
-      staff() {
-        this.$alert('这是一段内容', '标题名称', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$message({
-              type: 'info',
-              message: `action: ${ action }`
-            });
-          }
-        });
-      },
-      sendClick() {
+    async getlist () {
+      const { page, limit } = this
+      const res = await getAllNoticle({ page, limit })
+      this.tableData = res.data.rows
+      console.log(res.data)
+      this.total = res.data.count
+    },
+    async sendClick () {
+      console.log(this.form)
+      const res = await getTitle(this.form)
+      const { success } = res
+      if (!success) return this.$notify.warning('添加失败')
+      this.$notify.success('添加成功')
+      this.getList()
+      this.$message({
+        showClose: true,
+        message: '请输入公告内容',
+        type: 'warning'
+      })
+    },
+    rejectSite () {
+
+    },
+    staff () {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
           this.$message({
-          showClose: true,
-          message: '请输入公告内容',
-          type: 'warning'
-        });
-      },
-      rejectSite() {
-
-      },
-      loadNode(node, resolve) {
-        if (node.level === 0) {
-          return resolve([{ name: '全部' }]);
+            type: 'info',
+            message: `action:${action}`
+          })
         }
-        if (node.level > 1) return resolve([]);
-
-        setTimeout(() => {
-          const data = [{
-            name: '广州千锋网点',
-            leaf: true
-          }, {
-            name: '学三文化网点'
-          },{
-            name: '学三文化网点'
-          },{
-            name: '学三文化网点'
-          },{
-            name: '学三文化网点'
-          }];
-
-          resolve(data);
-        }, 500);
-      }
+      })
     }
-};
+  }
+}
 </script>
-<style>
+<style scoped>
     .el-input {
         width: 800px;
     }
