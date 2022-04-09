27fbf9c9bb8208f9e8e4fc595f31fbd06f1069b3
邹金城 <jonzou@163.com>
2022-04-10 00:18:40 +0800

    111

--- src/views/notice/sendNotice.vue
+++ src/views/notice/sendNotice.vue
@@ -13,8 +13,8 @@
         </el-radio-group>
       </el-form-item>
       <el-form-item label="内容">
-        <div @click="staff=true">
-            <el-input v-model="form.content" placeholder="请输入"></el-input>
+        <div @click="staff = true">
+          <el-input v-model="form.content" placeholder="请输入"></el-input>
         </div>
       </el-form-item>
       <el-form-item label="封面：">
@@ -27,7 +27,8 @@
           multiple
           :limit="3"
           :on-exceed="handleExceed"
-          :file-list="fileList">
+          :file-list="fileList"
+        >
           <el-button size="small" type="primary">选取封面</el-button>
         </el-upload>
       </el-form-item>
@@ -36,7 +37,7 @@
   </d2-container>
 </template>
 <script>
-import { getAllNoticle, addNoticle } from '@/apis/article'
+import { getAllNoticle, addNoticle } from '@/apis/article';
 export default {
   name: 'sendNotice',
   data () {
@@ -59,35 +60,33 @@ export default {
       },
       site: false,
       staff: false
-    }
+    };
   },
   created () {
-    this.getList()
+    this.getList();
   },
   methods: {
     async getlist () {
-      const { page, limit } = this
-      const res = await getAllNoticle({ page, limit })
-      this.tableData = res.data.rows
-      console.log(res.data)
-      this.total = res.data.count
+      const { page, limit } = this;
+      const res = await getAllNoticle({ page, limit });
+      this.tableData = res.data.rows;
+      console.log(res.data);
+      this.total = res.data.count;
     },
     async sendClick () {
-      console.log(this.form)
-      const res = await addNoticle(this.form)
-      const { success } = res
-      if (!success) return this.$notify.warning('发送失败')
-      this.$notify.success('发送成功')
-      this.getList()
+      console.log(this.form);
+      const res = await addNoticle(this.form);
+      const { success } = res;
+      if (!success) return this.$notify.warning('发送失败');
+      this.$notify.success('发送成功');
+      this.getList();
       // this.$message({
       //   showClose: true,
       //   message: '请输入公告内容',
       //   type: 'warning'
       // })
     },
-    rejectSite () {
-
-    },
+    rejectSite () {},
     staff () {
       this.$alert('这是一段内容', '标题名称', {
         confirmButtonText: '确定',
@@ -95,30 +94,30 @@ export default {
           this.$message({
             type: 'info',
             message: `action:${action}`
-          })
+          });
         }
-      })
+      });
     }
   },
   filters: {
     typeTxt (val) {
-      if (val === 1) return '寄件'
-      if (val === 2) return '收件'
-      if (val === 3) return '费用'
-      if (val === 4) return '招聘'
+      if (val === 1) return '寄件';
+      if (val === 2) return '收件';
+      if (val === 3) return '费用';
+      if (val === 4) return '招聘';
     }
   }
-}
+};
 </script>
 <style scoped>
-    .el-input {
-        width: 800px;
-    }
-    .el-dialog__header {
-        background: #f8f8f8;
-    }
-    .keyWord {
-        width: 300px;
-        margin-bottom: 30px;
-    }
+.el-input {
+  width: 800px;
+}
+.el-dialog__header {
+  background: #f8f8f8;
+}
+.keyWord {
+  width: 300px;
+  margin-bottom: 30px;
+}
 </style>
